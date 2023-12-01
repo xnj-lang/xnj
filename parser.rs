b37f41e5711a7d68c23d157ff3003bbc24034894
xinajjj <152647888+xinajjj@users.noreply.github.com>
2023-12-01 23:18:22 +0200

    Update parser.rs

--- parser.rs
+++ parser.rs
@@ -2,15 +2,22 @@ pub mod main{
     use std::process::Command;
     use std::fs::write;
     use std::fs::remove_file;
+    use std::fs::metadata;
 
     enum Macros {
         Output,
         Bracket,
         Quotes,
-        String
+        String,
+        Semicolon,
+        Main,
+        Ok
     }
     pub fn parse(code: &str){
-        let macros_vec: Vec<&str> = vec!["output", "(", ")", "\""];
+        if metadata("run.rs").is_ok() {
+            remove_file("run.rs").expect("Failed to remove file");
+        }
+        let macros_vec: Vec<&str> = vec!["output", "(", ")", "\"", ";", "main{", "Ok()"];
         let code = code.chars().collect::<Vec<char>>();
         let mut command = String::new();
         let mut opers: Vec<Option<Macros>> = Vec::new();
@@ -19,7 +26,16 @@ pub mod main{
         let mut curr_string = String::new();
         for char in code {
             command.push(char);
-            if string_oper && char != '"' {
+            if char.is_whitespace() {
+                if !command.trim().is_empty() {
+                    continue;
+                }
+                else{
+                    command.clear();
+                    continue;
+                }
+            }
+            else if string_oper && char != '"' {
                 if let Some(&Some(Macros::String)) = opers.last() {
                     curr_string.push(char);
                     command.clear();
@@ -34,12 +50,24 @@ pub mod main{
                         opers.push(Some(Macros::Output));
                         command.clear();
                     }
+                    "main{" => {
+                        opers.push(Some(Macros::Main));
+                        command.clear();
+                    }
                     "(" => {
                         opers.push(Some(Macros::Bracket));
                         command.clear();
                     }
                     ")" => {
                         opers.push(Some(Macros::Bracket));
+                        command.clear();
+                    }
+                    ";" => {
+                        opers.push(Some(Macros::Semicolon));
+                        command.clear();
+                    }
+                    "Ok()" => {
+                        opers.push(Some(Macros::Ok));
                         command.clear();
                     }
                     "\"" => {
@@ -58,15 +86,12 @@ pub mod main{
                 }
             }
         }
+
         let mut curr_oper = String::new();
-        curr_oper.push_str("fn main(){");
         let mut used = false;
-        for oper in opers {
-            if let Some(oper) = oper {
+        for operator in opers {
+            if let Some(oper) = operator {
                 match oper {
-                    Macros::Output => {
-                        curr_oper.push_str("print!");
-                    }
                     Macros::Bracket => {
                         if used == false {
                             curr_oper.push('(');
@@ -78,17 +103,18 @@ pub mod main{
                     }
                     Macros::String => {
                         curr_oper.push_str(&curr_string);
+                        curr_string.clear();
                     }
-                    Macros::Quotes => {
-                        curr_oper.push('"');
-                    }
+                    Macros::Main => curr_oper.push_str("fn main(){ \n"),
+                    Macros::Output => curr_oper.push_str("print!"),
+                    Macros::Quotes => curr_oper.push('"'),
+                    Macros::Semicolon => curr_oper.push_str(";\n"),
+                    Macros::Ok => curr_oper.push_str("print!(\"\nProcess finished with exit status Ok\")")
                 }
             }
         }
-        curr_oper.push(';');
         curr_oper.push('}');
         write("run.rs", curr_oper).expect("Failed to write file");
-
         let output = Command::new("rustc")
             .args(&["run.rs"])
             .output()
@@ -102,6 +128,5 @@ pub mod main{
         } else {
             eprintln!("Compilation failed: {:?}", String::from_utf8_lossy(&output.stderr));
         }
-        remove_file("run.rs").expect("Failed to remove file");
     }
 }
